e72ca617973f82da73e181e59a97cfed87923eea
Joseph Budina <joebud3@gmail.com>
2020-12-15 10:12:42 -0700

    Add it finds main character test

--- test/network_test.rb
+++ test/network_test.rb
@@ -6,9 +6,12 @@ require './lib/network'
 
 class NetworkTest < Minitest::Test
   def setup
+    @leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
+    @ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
     @kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
     @michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
     @knight_rider = Show.new("Knight Rider", "Glen Larson", [@michael_knight, @kitt])
+    @parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [@leslie_knope, @ron_swanson])    
     @nbc = Network.new("NBC")
   end
 
@@ -17,5 +20,19 @@ class NetworkTest < Minitest::Test
 
     assert_equal "NBC", @nbc.name
     assert_equal [], @nbc.shows
+  end
+
+  def test_it_can_add_shows
+    @nbc.add_show(@knight_rider)
+    @nbc.add_show(@parks_and_rec)
+
+    assert_equal [@knight_rider,@parks_and_rec], @nbc.shows
+  end
+
+  def test_it_can_find_main_characters
+    @nbc.add_show(@knight_rider)
+    @nbc.add_show(@parks_and_rec)
+
+    assert_equal [@kitt], @nbc.main_characters
   end
 end
