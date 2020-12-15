186a23444e0d3035dc43cf426b33bc07a23078ef
Joseph Budina <joebud3@gmail.com>
2020-12-15 10:13:12 -0700

    Add main characters method

--- lib/network.rb
+++ lib/network.rb
@@ -6,4 +6,18 @@ class Network
     @name = name
     @shows = []
   end
+
+  def add_show(show)
+    @shows << show
+  end
+
+  def main_characters
+    main = []
+    @shows.each do |show|
+      show.characters.each do |character|
+       main << character if character.salary > 500_000 && character.name == character.name.upcase
+      end
+    end
+    main
+  end
 end
